ccb812f6a50fc4d42e97583432854553552dadf5
Hyein <976hannah@snu.ac.kr>
2025-10-23 17:40:38 +0900

    Toy exponential function w/o symbolification -- inversion of score appears

--- github/workflows/Hyein/toy_4_exponential_tuning.py
+++ github/workflows/Hyein/toy_4_exponential_tuning.py
@@ -7,18 +7,20 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from kan.custom_utils import remove_outliers_iqr
 import json
+import datetime
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"This script is running on {device}.")
 
-x1_grid = np.linspace(-np.pi, np.pi, 30)
+x1_grid = np.linspace(-1, 1, 50)
 x2_grid = np.linspace(-1, 1, 30)
 
 x1, x2= np.meshgrid(x1_grid, x2_grid)
 X = np.stack((x1.flatten(), x2.flatten()), axis=1)
-# y = 10 * np.abs(x1) + 5*x2**2
-y = np.sin(2*x1) + x2
-eqn = "sin(2x0)+5x1"
+y = np.exp(-2*x1) + x2
+eqn = "e^(-2x0)+x1"
+save_heading = os.path.join(os.getcwd(), "github\workflows\Hyein\multkan_sweep_autosave",
+                            f"exponential_{datetime.datetime.now().strftime('%Y%m%d_%H%M')}")
 
 y = y.flatten().reshape(-1, 1)
 
@@ -44,48 +46,39 @@ X_test_norm = scaler_X.transform(X_test)
 y_val_norm = scaler_y.transform(y_val)
 y_test_norm = scaler_y.transform(y_test)
 
-# 딥러닝을 진행하기 전 모든 데이터셋을 tensor로 변환  # 원래는 numpy 배열이었음 --- 아까 scikitlearn의 train test split 이나 .fit transform 스케일러를 사용하였기에
-# X_train_tensor = torch.tensor(X_train_norm, dtype=torch.float32, device=device)
-# X_val_tensor = torch.tensor(X_val_norm, dtype=torch.float32, device=device)
-# X_test_tensor = torch.tensor(X_test_norm, dtype=torch.float32, device=device)
-# y_train_tensor = torch.tensor(y_train_norm, dtype=torch.float32, device=device)
-# y_val_tensor = torch.tensor(y_val_norm, dtype=torch.float32, device=device)
-# y_test_tensor = torch.tensor(y_test_norm, dtype=torch.float32, device=device)
-
-# y = df_out_final[name_y].values.reshape(-1, 1)
 out = sweep_multkan(
       X_train_norm, y_train_norm, X_val_norm, y_val_norm, X_test_norm, y_test_norm,
       param_grid={
           'width': [[X_train.shape[1], 2, 1]],
           'grid': [10],
           # 'grid_eps': [0.02, 0.5, 1],
+          # 'stop_grid_update_step': [20, 30, 100],
           'k': [3],
           'mult_arity': [0],
           'steps': [50],
           'opt': ['LBFGS'],
-          'lr': [0.1, 1, 10],
+          'lr': [0.05, 0.1, 0.5],
           'update_grid': [True],
-          'lamb': [1e-4, 1e-3, 1e-2],
-          'lamb_coef': [1],
+          'lamb': [1e-4],
+          'lamb_coef': [0.05, 0.1, 0.2],
           'lamb_entropy': [1],
           'prune': [True],
           'pruning_node_th': [0.01],
           'pruning_edge_th': [3e-2],
-          'symbolic': [False],
-          # 'sym_weight_simple': [0.5],
-          # 'sym_a_range': [(-50, 50)],
+          'symbolic': [True],
+          'sym_weight_simple': [0.9],
       },
-      seeds=[0, 17, 42, 87, 29],      # run each config with multiple seeds
+      seeds=[0, 17, 42],      # run each config with multiple seeds
       n_jobs=1,          # number of parallel worker processes
       use_cuda=False,     # set False to force CPU
-      eqn=eqn,
+      save_heading=save_heading,
   )
-print(out['results_avg_table'][['r2_val_mean', "param_lamb", "param_sym_weight_simple"]])
 best = out['best']
 print('Best configuration:')
 print(json.dumps(best, indent=2))
 
-res, model, _, _, save_info = evaluate_params(
-    X_train, y_train, X_val, y_val, best['params'], X_test, y_test, 0, scaler_y, device.type
+res, model, _, _ = evaluate_params(
+    X_train, y_train, X_val, y_val, best['params'], X_test, y_test, 0, scaler_y, device.type,
+    save_heading=save_heading
 )
-torch.save(model.state_dict(), os.path.join(save_info["dir"], f"{save_info['tag']}_model.pt"))
+torch.save(model.state_dict(), f"{save_heading}_model.pt")
