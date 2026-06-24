76948495aab8a87a4655f05958cb7c3779b00dce
Hyein <976hannah@snu.ac.kr>
2026-06-24 13:45:59 +0900

    KAN interval analysis for a conditional function

--- github/workflows/Hyein/toy_KAN_analyze.py
+++ github/workflows/Hyein/toy_KAN_analyze.py
@@ -72,7 +72,7 @@ SA_RC = {
 
 def main():
     parser = argparse.ArgumentParser(description="Tune KAN for Analytical Functions.")
-    parser.add_argument("func_name", type=str, nargs='?', default="exponential",
+    parser.add_argument("func_name", type=str, nargs='?', default="conditional",
                         choices=FUNCTION_ZOO.keys(),
                         help="Choose a function from the ZOO.")
 
